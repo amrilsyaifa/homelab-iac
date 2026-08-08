f61d7aeed340795ffa4a996cb796bab11c9f46eb
iamrils <amrilsyaifa10@gmail.com>
2026-08-08 20:18:37 +0700

    feat: add MinIO, Gitea and Docker Registry

---
+++ docker/create-network.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+NETWORK_NAME="homelab"
+
+if docker network inspect "$NETWORK_NAME" >/dev/null 2>&1; then
+  echo "Docker network '$NETWORK_NAME' already exists."
+else
+  docker network create "$NETWORK_NAME"
+  echo "Docker network '$NETWORK_NAME' created."
+fi
